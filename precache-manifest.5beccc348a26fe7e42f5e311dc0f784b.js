79ea986cbe30d4d2d82908cf0649edf2f2a31845
Student <ziwei.polaris@gmail.com>
2019-08-12 16:55:07 +0100

    Updates

---
+++ precache-manifest.5beccc348a26fe7e42f5e311dc0f784b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122454ea3e889f74284e36b8fdf4c581",
+    "url": "/frontend-api-challenge/index.html"
+  },
+  {
+    "revision": "efbadfffa2b58d4ab5ec",
+    "url": "/frontend-api-challenge/static/css/main.65ca9caa.chunk.css"
+  },
+  {
+    "revision": "32f77cbe909c587be667",
+    "url": "/frontend-api-challenge/static/js/2.a76e6113.chunk.js"
+  },
+  {
+    "revision": "efbadfffa2b58d4ab5ec",
+    "url": "/frontend-api-challenge/static/js/main.9d0bc706.chunk.js"
+  },
+  {
+    "revision": "f5dc398538ca2ba04cac",
+    "url": "/frontend-api-challenge/static/js/runtime~main.51d2e0b4.js"
+  },
+  {
+    "revision": "4ee5f938470002dd2225faa9d11dfbdb",
+    "url": "/frontend-api-challenge/static/media/acorn.4ee5f938.svg"
+  },
+  {
+    "revision": "ca58fb5a5b3d9ac05d98a151a2536843",
+    "url": "/frontend-api-challenge/static/media/squiggle_default.ca58fb5a.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/frontend-api-challenge/precache-manifest.5beccc348a26fe7e42f5e311dc0f784b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend-api-challenge/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
